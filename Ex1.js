e34415b311df5f126735293fdda54107e5005907
Arthur Emanuel <arthurresende2004@gmail.com>
2024-07-24 04:15:02 -0300

    Primeiro exercicio

--- Ex1.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')();
-
-var num1 = parseFloat(prompt("Digite o primeiro Numero: "))
-var num2 = parseFloat(prompt("Digite o segundo Numero: "))
-
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
