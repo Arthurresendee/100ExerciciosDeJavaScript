f4210883b212ab532e6dbe33d297368aa0c3a6f1
Arthur Emanuel <arthurresende2004@gmail.com>
2024-07-24 04:39:47 -0300

    Terceiro exercicio

---
+++ Ex1.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+var num1 = parseFloat(prompt("Digite o primeiro Numero: "))
+var num2 = parseFloat(prompt("Digite o segundo Numero: "))
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
--- Ex3.js
+++ Ex3.js
@@ -2,5 +2,6 @@ const prompt = require('prompt-sync')();
 
 var num1 = parseFloat(prompt("Digite o primeiro numero: "))
 var num2 = parseFloat(prompt("Digite o segundo numero: "))
+var num3 = parseFloat(prompt("Digite o terceiro numero: "))
 
-console.log("A média é: ",(num1 + num2) / 2 )
+console.log("A média é: ",(num1 + num2 + num3) / 3 )
