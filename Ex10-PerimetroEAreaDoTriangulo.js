a0b1d4a28bc825ba282320b962a7e37ff7665341
Arthur Emanuel <arthurresende2004@gmail.com>
2024-07-25 23:59:39 -0300

    Exercicio de numero 10

---
+++ Ex10-PerimetroEAreaDoTriangulo.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 5;
+var c = 5;
+
+var h = 4;
+
+var perimetro = a + b + c;
+var area = (b*h)/2;
+
+console.log("O perímetro é: "+ perimetro + "cm")
+console.log("A área é: "+ area + "cm")
