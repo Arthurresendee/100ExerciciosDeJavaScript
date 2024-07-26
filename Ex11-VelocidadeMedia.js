79c7f96c18198ef2e5a97c0f4f3cbeeff3d8ab67
Arthur Emanuel <arthurresende2004@gmail.com>
2024-07-26 00:39:21 -0300

    Exercicio de numero 11

---
+++ Ex11-VelocidadeMedia.js
@@ -0,0 +1,7 @@
+var deltaS = 30000;
+var deltaT = 200;
+
+var velocidadeMedia = deltaS/deltaT
+
+console.log("Você percorreu " + deltaS + "m em "+ deltaT + " minutos")
+console.log("Sua velocidade média foi de: " + velocidadeMedia + "m/s");
